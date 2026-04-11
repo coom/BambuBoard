49e9f659e62aba8cddb888f75190f8b55fa930ff
coom <coom@e-odyssey.net>
2026-04-11 17:47:58 +0200

    fix: include backend/config.py so addon can import config module

---
+++ backend/config.py
@@ -0,0 +1,10 @@
+import os
+
+PRINTER_IP = os.environ.get("PRINTER_IP", "")
+PRINTER_SERIAL = os.environ.get("PRINTER_SERIAL", "")
+PRINTER_ACCESS_CODE = os.environ.get("PRINTER_ACCESS_CODE", "")
+
+HA_WEBHOOK_URL = os.environ.get("HA_WEBHOOK_URL", "")
+HA_WEBHOOK_TOKEN = os.environ.get("HA_WEBHOOK_TOKEN", "")
+
+LOW_STOCK_THRESHOLD = int(os.environ.get("LOW_STOCK_THRESHOLD", "20"))
